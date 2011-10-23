4f29dad54b71d2f6e887cf3dd583fe98b00bbdc6
ismaeldm <ismaeldm@gmail.com>
2011-10-23 09:49:59 +0200

    Initial Commit

---
+++ LetrasSinCifras/IDMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDMAppDelegate.h
+//  LetrasSinCifras
+//
+//  Created by Delgado Molina Ismael on 23/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LetrasSinCifras/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LetrasSinCifras
+//
+//  Created by Delgado Molina Ismael on 23/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDMAppDelegate class]));
+    }
+}
